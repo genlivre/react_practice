bfda13aa7234534a2262f5a4b01aad9d2b9aad68
Genta <>
2020-11-10 18:48:32 +0900

    コンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div className="App"><p>Hello World !</p></div>;
+// import React, { Component } from "react";
+
+// class App extends Component {
+//   render() {
+//     const dom = <h1>Hello World !!</h1>;
+//     return <React.Fragment>
+//       {dom}
+//     </React.Fragment>;
+//   }
+// }
+
+const App = () => {
+  return <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>;
+}
+
+const Cat = () => {
+  return <p>みゃおん</p>
 }
 
 export default App;
